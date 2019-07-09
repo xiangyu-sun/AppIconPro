2c8f10d32fd0f5d519eedc8f08146ddca1f58901
Xiangyu Sun <xiangyu.sun@dnata.com>
2019-07-09 19:57:06 +0400

    Initial Commit

---
+++ AppIconPro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppIconPro
+//
+//  Created by 孙翔宇 on 09/07/2019.
+//  Copyright © 2019 孙翔宇. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppIconPro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppIconPro
+//
+//  Created by 孙翔宇 on 09/07/2019.
+//  Copyright © 2019 孙翔宇. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
